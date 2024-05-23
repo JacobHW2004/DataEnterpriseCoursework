be33d34e1d7681860ce9cc620691e25f416f82f2
JacobHW2004 <110376542+JacobHW2004@users.noreply.github.com>
2024-05-23 14:57:46 +0100

    Delete nsi-units directory

--- nsi-units/si-units/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/commandchecker/commandchecker/nsi-units/si-units
-# Build directory: D:/commandchecker/commandchecker/build/nsi-units/si-units
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
