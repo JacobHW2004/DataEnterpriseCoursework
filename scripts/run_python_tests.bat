d22638429ab76cd5bbe04b0af445c43ee7aeccaf
JacobHW2004 <110376542+JacobHW2004@users.noreply.github.com>
2024-05-24 13:46:05 +0100

    Delete scripts directory

--- scripts/run_python_tests.bat
+++
@@ -1,9 +0,0 @@
-echo "Running unit tests using nose"
-cd DataExporter
-nosetests --with-xunit
-echo "Running unit tests along with coverage"
-coverage run -m unittest discover -s Test/
-echo "getting coverage report"
-coverage report -m
-coverage xml
-coverage html
